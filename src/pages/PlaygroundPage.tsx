6a79901268bca6afa5caca600c6b8f23d45c8660
senthil-k8s <senthilathiban0@gmail.com>
2024-06-04 18:53:37 +0530

    project setup

---
+++ src/pages/PlaygroundPage.tsx
@@ -0,0 +1,9 @@
+
+import ExpandableText from "../components/ExpandableText";
+
+const PlaygroundPage = () => {
+  const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sint dolores neque fugit velit mollitia excepturi sunt delectus ducimus unde. Magnam, quod! Vitae ipsa veritatis, sit laborum laudantium dolorem quam magni nemo eaque recusandae atque maxime! Eaque eos provident odio numquam, molestiae iste, maxime doloribus est soluta inventore quia amet quos deleniti sapiente voluptate quaerat dicta! Voluptatum quasi repudiandae similique quas voluptate nulla magnam, suscipit cum doloremque dolorum vero nostrum minima laborum quisquam est mollitia tenetur laboriosam blanditiis maxime molestias eaque ex, sit quaerat debitis. Ad repellat officia voluptas, mollitia nihil quam temporibus voluptatem omnis optio aperiam deleniti dolor quod.";
+  return <ExpandableText text={text} />;
+};
+
+export default PlaygroundPage;
